dc098fa8e4c96776c7d7cdd5bf3b2821e7c3374e
Adriano Pavao <adribenig@hotmail.com>
2024-03-19 23:09:20 -0300

    Ajustes integracão cognito

--- src/main/java/com/yourcompany/CPFVerificationLambda.java
+++ src/main/java/com/yourcompany/CPFVerificationLambda.java
@@ -1,16 +1,34 @@
 package com.yourcompany;
 
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
+import com.amazonaws.services.cognitoidp.model.AdminInitiateAuthResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import jakarta.enterprise.context.ApplicationScoped;
+
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.*;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import jakarta.inject.Named;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 @Named("cpfVerification")
 @ApplicationScoped
@@ -43,7 +61,7 @@ public class CPFVerificationLambda implements RequestHandler<APIGatewayProxyRequ
         if (result.item() == null || result.item().isEmpty()) {
 
             APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-            response.setStatusCode(404);
+            response.setStatusCode(401);
             response.setBody("CPF not found: " + cpf);
             return response;
 
@@ -51,16 +69,66 @@ public class CPFVerificationLambda implements RequestHandler<APIGatewayProxyRequ
             // Here you should integrate with Amazon Cognito to retrieve an access token
             // This is a placeholder for where you would add that logic
 
+            String clientId = "4daih53dr47miaualue9p8e14k";
+
+            String username = "usuario2"; // Replace with a valid username
+            String password = "PlaPla3030"; 
+
+
+
+             CognitoIdentityProviderClient cognitoClient = CognitoIdentityProviderClient.builder()
+                    .region(Region.US_EAST_1) // Substitua pela região do seu User Pool
+                    .httpClient(UrlConnectionHttpClient.create())
+                    .build();
+
+
+
+
+
+                  
+
+            AdminInitiateAuthRequest authRequest = AdminInitiateAuthRequest.builder()
+                    .clientId(clientId)
+                    .authFlow(AuthFlowType.ADMIN_USER_PASSWORD_AUTH)
+                    .userPoolId("us-east-1_Ny9FfbR8y")
+                    .authParameters(Map.of("USERNAME", username, "PASSWORD", password))
+                    .build();
+
+                   
+
+            AdminInitiateAuthResponse authResponse = cognitoClient.adminInitiateAuth(authRequest);
+
+
+
+            String accessToken = authResponse.authenticationResult().accessToken();
       
 
             APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
             response.setStatusCode(200);
             response.setBody("CPF verification result: " + cpf);
+            response.setHeaders(Map.of("Authorization", accessToken));
             return response;
 
         }
 
 
         
+    }
+
+    public static String calculateSecretHash(String userPoolClientId, String userPoolClientSecret, String userName) {
+        final String HMAC_SHA256_ALGORITHM = "HmacSHA256";
+        SecretKeySpec signingKey = new SecretKeySpec(
+                userPoolClientSecret.getBytes(StandardCharsets.UTF_8), HMAC_SHA256_ALGORITHM);
+        try {
+            Mac mac = Mac.getInstance(HMAC_SHA256_ALGORITHM);
+            mac.init(signingKey);
+            mac.update(userName.getBytes(StandardCharsets.UTF_8));
+            byte[] rawHmac = mac.doFinal(userPoolClientId.getBytes(StandardCharsets.UTF_8));
+            //String hash = Base64.getEncoder().encodeToString(mac.doFinal((userName + userPoolClientId).getBytes()));
+            
+            return Base64.getEncoder().encodeToString(rawHmac);
+        } catch (Exception e) {
+            throw new RuntimeException("Error while calculating ");
+        }
     }
 }
